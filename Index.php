b018f9a0a8b54c42cd9c2ba2f34c09056f1fe4c3
mattprice28 <pricem20@cardiff.ac.uk>
2017-03-31 17:04:35 +0100

    continued work
    added dropdown list for make and submit button

--- Index.php
+++ Index.php
@@ -7,14 +7,26 @@
 		</head>
 		<div class = "window">
 			<body>
-				<form action="vehicle_page.php">
+				<form id ="vehicle" action="vehicle_page.php">
 					<h2>Choose your Vehicle<h2><br>
 					<input list="Make">
 					<datalist id ="Make">
 						<option value="Toyota">
 						<option value="Other">
+					</datalist><br>
+					<input list="Model">
+					<datalist id ="Model">
+						<option value="Supra">
+						<option value="Other">
 					</datalist>
+					<br>
+					<input type="button" onclick="selectVehicle()" value="Submit">	
 				</form>
+				<script>
+				function selectVehicle() {
+					document.getElementbyID("vehicle").submit();
+				}
+				</script>
 			</body>
 		</div>
 	</hmtl>
--- vehicle_page.php
+++ vehicle_page.php
@@ -26,6 +26,68 @@
 			<body>
 				<script src="js/three.js"></script>
 				<script>
+				// The detector will show a warning if the current browser does not support WebGL.
+if (!Detector.webgl) {
+    Detector.addGetWebGLMessage();
+}
+
+// All of these variables will be needed later, just ignore them for now.
+var container;
+var camera, controls, scene, renderer;
+var lighting, ambient, keyLight, fillLight, backLight;
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+init();
+animate();
+
+function init() {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    // Code...
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.z = 3;
+scene = new THREE.Scene();
+ambient = new THREE.AmbientLight(0xffffff, 1.0);
+scene.add(ambient);
+
+var mtlLoader = new THREE.MTLLoader();
+mtlLoader.setBaseUrl('models/');
+mtlLoader.setPath('models/');
+mtlLoader.load('Supra3D.mtl', function (materials) {
+
+    materials.preload();
+
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setMaterials(materials);
+    objLoader.setPath('models/');
+    objLoader.load('Supra3D.obj', function (object) {
+
+        scene.add(object);
+
+    });
+
+});
+}
+
+function render() {
+    // Code...
+    renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(new THREE.Color("hsl(0, 0%, 10%)"));
+
+container.appendChild(renderer.domElement);
+function render() {
+    requestAnimationFrame(render);
+    controls.update();
+    renderer.render(scene, camera);
+}
+controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.enableZoom = false;
+}
 				</script>
 				<ul class = "parts">
 					<li>Front Bumper</li>
